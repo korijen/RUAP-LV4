9786fb71222608aa982f1db911fbb8512d2537f6
Bruno Pandza <bruno.pandza99@gmail.com>
2021-12-06 21:41:47 +0100

    Fourth test- Logging in

---
+++ test5/RUAP-LV4/RUAP-LV4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RUAP-LV4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RUAP-LV4")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0f71ce4-f7f4-444e-a3fc-3e7ed4dfd1a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test5/RUAP-LV4/RUAP-LV4/UnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace RUAP_LV4
+{
+    namespace SeleniumTests
+    {
+        [TestFixture]
+        public class UntitledTestCase
+        {
+            private IWebDriver driver;
+            private StringBuilder verificationErrors;
+            private string baseURL;
+            private bool acceptNextAlert = true;
+
+            [SetUp]
+            public void SetupTest()
+            {
+                driver = new FirefoxDriver();
+                baseURL = "https://www.google.com/";
+                verificationErrors = new StringBuilder();
+            }
+
+            [TearDown]
+            public void TeardownTest()
+            {
+                try
+                {
+                    driver.Quit();
+                }
+                catch (Exception)
+                {
+                    // Ignore errors if unable to close the browser
+                }
+                
+            }
+
+            [Test]
+            public void TheUntitledTestCaseTest()
+            {
+                driver.Navigate().GoToUrl("http://demowebshop.tricentis.com/");
+                driver.Navigate().GoToUrl("http://demowebshop.tricentis.com/");
+                driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Register'])[1]/following::li[1]")).Click();
+                driver.FindElement(By.LinkText("Log in")).Click();
+                driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='New Customer'])[1]/following::div[4]")).Click();
+                driver.FindElement(By.Id("Email")).Click();
+                driver.FindElement(By.Id("Email")).Clear();
+                driver.FindElement(By.Id("Email")).SendKeys("bruno.pandza99@gmail.com");
+                driver.FindElement(By.Id("Password")).Clear();
+                driver.FindElement(By.Id("Password")).SendKeys("Bucikralj1");
+                driver.FindElement(By.XPath("//input[@value='Log in']")).Click();
+            }
+            private bool IsElementPresent(By by)
+            {
+                try
+                {
+                    driver.FindElement(by);
+                    return true;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            }
+
+            private bool IsAlertPresent()
+            {
+                try
+                {
+                    driver.SwitchTo().Alert();
+                    return true;
+                }
+                catch (NoAlertPresentException)
+                {
+                    return false;
+                }
+            }
+
+            private string CloseAlertAndGetItsText()
+            {
+                try
+                {
+                    IAlert alert = driver.SwitchTo().Alert();
+                    string alertText = alert.Text;
+                    if (acceptNextAlert)
+                    {
+                        alert.Accept();
+                    }
+                    else
+                    {
+                        alert.Dismiss();
+                    }
+                    return alertText;
+                }
+                finally
+                {
+                    acceptNextAlert = true;
+                }
+            }
+        }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
